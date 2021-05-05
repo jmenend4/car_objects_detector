e90deeeafa05766104e34c9d4ed2fe93b4cd67fa
jmenend4 <jmenend4@gmail.com>
2021-05-05 17:31:51 -0300

    commented uncompleted service workers code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { Provider as ReduxProvider } from "react-redux";
 
 const store = configureStore();
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./sw.js")
-    .then(console.log)
-    .catch(console.error);
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("./sw.js")
+//     .then(console.log)
+//     .catch(console.error);
+// }
 
 render(
   <ReduxProvider store={store}>
